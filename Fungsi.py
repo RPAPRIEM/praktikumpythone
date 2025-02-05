875166358b039c799d47178fc598944553d636d8
Rehan Prima Abdillah <rehanprimaa27@gmail.com>
2025-02-05 13:22:38 +0700

    Add files via upload

---
+++ Fungsi.py
@@ -0,0 +1,11 @@
+def persegi_panjang(p, l, t):
+    return p*l*t
+
+p = float(input("masukkan nilai panjang: "))
+l = float(input("masukkan nilai lebar: "))
+t = float(input("masukkan nilai tinggi: "))
+luas = persegi_panjang(p, l, t)
+print(f"luas persegi panjang {p, l, t} adalah {luas}")
+
+
+        
